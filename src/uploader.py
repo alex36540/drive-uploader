1176babccdfeb16ff16f20a597946795225158a9
alex36540 <alex36540@gmail.com>
2021-06-12 19:39:50 -0400

    get_drive_dict developed

--- src/uploader.py
+++ src/uploader.py
@@ -16,13 +16,34 @@ def drive_auth():
 
 
 def get_drive_dict(folder_id, drive):
+    """
+    Returns a dictionary of how many files of each acronym are in the folder
+
+    :param folder_id: ID of folder to parse
+    :param drive: Google Drive authenticated object
+    """
     file_list = drive.ListFile({'q': "'{}' in parents and trashed=false".format(folder_id)}).GetList()
 
     drive_dict = dict()
 
     for file in file_list:
         if file['mimeType'] != 'application/vnd.google-apps.folder':
-            print('Title: %s, ID: %s' % (file['title'], file['id']))
+            title = file['title'].split('.')[0]
+            num_index = 0
+
+            # find the first number in the title and split
+            while title[num_index].isalpha():
+                num_index += 1
+
+            acronym = title[:num_index]
+            count = int(title[num_index:])
+
+            # insert value in dictionary accordingly
+            if acronym in drive_dict:
+                if drive_dict[acronym] < count:
+                    drive_dict[acronym] = count
+            else:
+                drive_dict[acronym] = count
 
     return drive_dict
 
@@ -71,7 +92,9 @@ def main():
 
     src = r'D:\Downloads\Images'
 
-    gui(src, drive_dict)
+    print(drive_dict)
+
+    # gui(src, drive_dict)
 
 
 if __name__ == '__main__':
