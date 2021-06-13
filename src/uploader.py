c3e5639f38993825146677da405b8c42d53f8b96
alex36540 <alex36540@gmail.com>
2021-06-12 20:56:10 -0400

    video player added

--- src/uploader.py
+++ src/uploader.py
@@ -1,6 +1,7 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
+import threading
 from tkinter import *
 from PIL import ImageTk, Image
 import imageio
@@ -70,17 +71,47 @@ def gui(src, img_dict):
     # Text entry field
     name = StringVar()
 
+    thread = threading.Thread()
+    thread_on = False
+
+    def stream_vid(label_obj, vid):
+        """
+        Streams a video frame by frame in a label Tkinter object
+
+        :param label_obj: Tkinter label
+        :param vid: video to stream
+        """
+        while True:
+            for image in vid.iter_data():
+                if not thread_on:
+                    return
+
+                frame = ImageTk.PhotoImage(Image.fromarray(image))
+                label_obj.config(image=frame)
+                label_obj.frame_ref = frame
+
     def submit(event):
-        nonlocal count
+        """
+        
+        """
+        nonlocal count, thread, thread_on
 
         name.set('')
         count += 1
 
         next_name = img_list[count]
 
-        # if it's a video
+        # if video
         if next_name.split('.')[1] == 'mp4':
-            video = imageio.get_reader(img)
+            video = imageio.get_reader(join(src, next_name))
+
+            thread = threading.Thread(target=stream_vid, args=(label, video))
+            thread_on = True
+            thread.start()
+            return
+        else:
+            if thread_on:
+                thread_on = False
 
         next_img = ImageTk.PhotoImage(Image.open(join(src, next_name)))
         label.config(image=next_img)
